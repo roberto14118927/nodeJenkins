6b24d25b32e2530a5b8ed4e2aa0e3139d890f67a
robertoruiz14118927 <robertoruiz141189@gmail.com>
2022-04-04 20:34:08 -0500

    add docker

---
+++ nodejsDockerDSL.groovy
@@ -0,0 +1,28 @@
+job('Aplicacion Node.js Docker DSL') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('macloujulian')
+            node / gitConfigEmail('macloujulian@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('macloujulian/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+
+    }
+}
